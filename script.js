c0a360540f783f5b017785aa018eaa9fcc2ba20d
Luiz Esposito <neto.esposito@gmail.com>
2024-06-20 19:06:20 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const containers = document.getElementsByClassName('container')
 let indexContainers = 0
-
 const rightArrow = document.getElementById('right-arrow')
 const leftArrow = document.getElementById('left-arrow')
+const moreInfo = document.getElementsByClassName('more-info')
+leftArrow.style.opacity = 0.3
 
-rightArrow.addEventListener('click', () => {
-
+function goToNextPage () {
     containers[indexContainers].classList.remove('ativa')
-    indexContainers++
+    if (indexContainers < containers.length - 1) indexContainers++
+    if (indexContainers === containers.length - 1) rightArrow.style.opacity = 0.3
+    leftArrow.style.opacity = 1
     containers[indexContainers].classList.add('ativa')
 
-    if(indexContainers === containers.length - 1) {
-        rightArrow.style.opacity = 0.3
-    }
-})
-leftArrow.addEventListener('click', () => {
     if (indexContainers === 0) {
-        return;
+        moreInfo[indexContainers].classList.add('red');
+    } else if (indexContainers === 1) {
+        moreInfo[indexContainers].classList.add('yellow');
+    } else if (indexContainers === 3) {
+        moreInfo[indexContainers].classList.add('blue');
     }
-    if (indexContainers >= 0) {
-        rightArrow.style.opacity = 1
-    }
-    containers[indexContainers].classList.add('invisible')
-    indexContainers--
-    containers[indexContainers].classList.remove('invisible')
+}
+function goToPreviousPage () {
+    containers[indexContainers].classList.remove('ativa')
+    if (indexContainers > 0) indexContainers--
+    if (indexContainers === 0) leftArrow.style.opacity = 0.3
+    rightArrow.style.opacity = 1
+    containers[indexContainers].classList.add('ativa')
 
-})
+    if (indexContainers === 0) {
+        moreInfo[indexContainers].classList.add('red');
+    } else if (indexContainers === 1) {
+        moreInfo[indexContainers].classList.add('yellow');
+    } else if (indexContainers === 3) {
+        moreInfo[indexContainers].classList.add('blue');
+    }
+}
+
+
+
 
