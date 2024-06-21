d10744a6a90040c2153537d22be8ab51c2371332
Luiz Esposito <neto.esposito@gmail.com>
2024-06-21 12:58:01 -0300

    att 21/06/24

--- script.js
+++ script.js
@@ -3,39 +3,37 @@ let indexContainers = 0
 const rightArrow = document.getElementById('right-arrow')
 const leftArrow = document.getElementById('left-arrow')
 const moreInfo = document.getElementsByClassName('more-info')
-leftArrow.style.opacity = 0.3
+function loadInitialStates() {
+    leftArrow.style.opacity = 0.3
+}
 
-function goToNextPage () {
+function goToNextPage() {
     containers[indexContainers].classList.remove('ativa')
     if (indexContainers < containers.length - 1) indexContainers++
     if (indexContainers === containers.length - 1) rightArrow.style.opacity = 0.3
     leftArrow.style.opacity = 1
     containers[indexContainers].classList.add('ativa')
-
-    if (indexContainers === 0) {
-        moreInfo[indexContainers].classList.add('red');
-    } else if (indexContainers === 1) {
-        moreInfo[indexContainers].classList.add('yellow');
-    } else if (indexContainers === 3) {
-        moreInfo[indexContainers].classList.add('blue');
-    }
+    const moreInfoColor = changeColorMoreInfo()
+    moreInfo[indexContainers].classList.add(moreInfoColor)
 }
-function goToPreviousPage () {
+function goToPreviousPage() {
     containers[indexContainers].classList.remove('ativa')
     if (indexContainers > 0) indexContainers--
     if (indexContainers === 0) leftArrow.style.opacity = 0.3
     rightArrow.style.opacity = 1
     containers[indexContainers].classList.add('ativa')
-
-    if (indexContainers === 0) {
-        moreInfo[indexContainers].classList.add('red');
-    } else if (indexContainers === 1) {
-        moreInfo[indexContainers].classList.add('yellow');
-    } else if (indexContainers === 3) {
-        moreInfo[indexContainers].classList.add('blue');
-    }
+    const moreInfoColor = changeColorMoreInfo()
+    moreInfo[indexContainers].classList.add(moreInfoColor)
 }
-
+function changeColorMoreInfo() {
+    const Colors = {
+        0: 'red',
+        1: 'yellow',
+        2: 'orange',
+        3: 'blue'
+    }
+    return Colors[indexContainers]
+}
 
 
 
